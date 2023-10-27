b859962cb119a8649e309adf051bfc5653fbf0a1
MohamadMehdiK <MohamadMehdiK@SystemGroup.Net>
2023-10-27 23:20:54 +0330

    Class name is changed.

--- src/com/mmkarami/spring/CompanyApp.java
+++ src/com/mmkarami/spring/CompanyApp.java
@@ -2,13 +2,14 @@ package com.mmkarami.spring;
 
 import java.util.Date;
 
+import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-public class Company {
+public class CompanyApp {
 
 	public static void main(String[] args) {
 
-		try (ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml")) {
+		try (AbstractApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml")) {
 			Person person = context.getBean("employee", Person.class);
 			person.printBill(new Bill(new Date(), "This is the content of Bill."));
 		}
